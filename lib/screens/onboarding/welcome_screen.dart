55d6fefeb07979a08705052337bc280e8077a09d
Sandra Aframea Ofosu <168097776+Aframia@users.noreply.github.com>
2025-12-12 13:09:43 +0000

    Update l10n output path and imports

--- lib/screens/onboarding/welcome_screen.dart
+++ lib/screens/onboarding/welcome_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:nexus_fertility_app/flutter_gen/gen_l10n/app_localizations.dart';
 import 'account_type_selection_screen.dart';
 import 'login_screen.dart';
 
